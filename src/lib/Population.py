3e7663d0deb85bdc666c9b37ab5f3e4838184077
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-11-28 07:15:48 -0600

    implement population mating procedure

--- src/lib/Population.py
+++ src/lib/Population.py
@@ -1,4 +1,6 @@
 
+import random
+
 class Population:
 
     def __init__(self, clazz, survivors=4, childrenPerSurvivor=2):
@@ -10,7 +12,7 @@ class Population:
         for i in range(self.survivors):
             self.currentGenerataion.append(clazz())
 
-        # self.mateGeneration()
+        self.mateGeneration()
     
     def stats(self):
         totalCount = len(self.currentGenerataion)
@@ -23,16 +25,24 @@ class Population:
                 maxFitness = v.fitness()
         
         return {"avg": totalFitness/totalCount, "max": maxFitness}
-        
-
 
     def cullGeneration(self):
         """ retain only the top survivors for this generation """
         raise NotImplementedError("Please Implement this method")
 
     def mateGeneration(self):
-        """ mate the survivors randomly based on childrenPerSurvivor """
-        raise NotImplementedError("Please Implement this method")
+        generation = self.currentGenerataion
+        newGeneration = []
+        
+        for v in self.currentGenerataion:
+            for i in range(self.childrenPerSurvivor):
+                other = random.choice(generation)
+                while other == v:
+                    other = random.choice(generation)
+                newGeneration.append(v.mate(other))
+            newGeneration.append(v)
+        
+        self.currentGenerataion = newGeneration
 
     def simulateGenerations(self, count=5):
         for i in range(count):
--- src/main.py
+++ src/main.py
@@ -4,17 +4,10 @@ from lib.WheightedGeneticEntity import WheightedGeneticEntity
 from lib.Population import Population
 
 if __name__ == "__main__":
-    # population = Population(WheightedGeneticEntity)
+    population = Population(WheightedGeneticEntity)
+    print(f"gen{0}", population.stats())
 
     # for i in range(5):
     #     print(f"gen{i}", population.stats())
     #     population.simulateGenerations(1)
     
-    x1 = WheightedGeneticEntity()
-    x2 = WheightedGeneticEntity()
-    c0 = x1.mate(x2)
-
-    print(x1.genes)
-    print(x2.genes)
-    print(c0.genes)
-
