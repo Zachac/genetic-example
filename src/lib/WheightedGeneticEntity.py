ed7650d11b8d47148939f2fa795a7c75dc1e8761
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-11-28 07:08:00 -0600

    implement mating function

--- src/lib/WheightedGeneticEntity.py
+++ src/lib/WheightedGeneticEntity.py
@@ -7,11 +7,11 @@ wheights = [0.9, -0.4, -0.8, 0.6, -0.4, 0.1]
 class WheightedGeneticEntity(GeneticEntity):
     
 
-    def __init__(self):
-        self.genes = []
+    def __init__(self, genes=None):
+        self.genes = genes or []
 
-        for i in range(len(wheights)):
-            self.genes.append(random.randint(-1, 1))
+        for i in range(len(self.genes), len(wheights)):
+            self.genes.append(random.uniform(-1, 1))
 
     def fitness(self):
         fitness = 0
@@ -22,6 +22,20 @@ class WheightedGeneticEntity(GeneticEntity):
         return fitness / len(wheights)
 
     def mate(self, other):
-        """ Return a new instance of this class that is the result
-            of mating these two instances """
-        raise NotImplementedError("Please Implement this method")
+        newGenes = []
+
+        for i in range(len(wheights)):
+            selectedGene = None
+
+            if random.getrandbits(1):
+                selectedGene = self.genes[i]
+            else:
+                selectedGene = other.genes[i]
+
+            if random.random() < 0.1:
+                selectedGene = random.uniform(-1, 1)
+
+            newGenes.append(selectedGene)
+
+        return WheightedGeneticEntity(genes=newGenes)
+
